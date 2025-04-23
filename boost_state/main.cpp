2889c8ad363431a0b580367416c476d21352e0f5
chenghao.pan <panchenghao@gaozhe.com.cn>
2025-04-23 14:32:12 +0800

    send rss

--- boost_state/main.cpp
+++ boost_state/main.cpp
@@ -8,6 +8,7 @@
 #include <boost/statechart/state.hpp>
 #include <iostream>
 
+#ifdef DEEP_HISTORY
 namespace sc = boost::statechart;
 
 // 事件
@@ -46,7 +47,6 @@ struct Shutdown : sc::simple_state<Shutdown, Robot> {
     Shutdown() { std::cout << "Robot is Shutdown\n"; }
      typedef sc::transition<EvRestart, sc::deep_history<Moving>> reactions; // 必须指定为组下的子状态。并不是回到Moving
 };
-
 // 移动状态
 struct Moving : sc::simple_state<Moving, Operational> {
     Moving() { std::cout << "Robot is Moving\n"; }
@@ -117,147 +117,178 @@ int main() {
 
     return 0;
 }
+#endif
 /////////////////////////正交
 
 
-// #include <iostream>
-// #include <boost/statechart/state_machine.hpp>
-// #include <boost/statechart/simple_state.hpp>
-// #include <boost/statechart/transition.hpp>
-// #include <boost/statechart/custom_reaction.hpp>
-// #include <boost/statechart/event.hpp>
-// #include <boost/mpl/list.hpp>
+#include <iostream>
+#include <boost/statechart/state_machine.hpp>
+#include <boost/statechart/simple_state.hpp>
+#include <boost/statechart/transition.hpp>
+#include <boost/statechart/custom_reaction.hpp>
+#include <boost/statechart/event.hpp>
+#include <boost/mpl/list.hpp>
 
-// namespace sc = boost::statechart;
-// namespace mpl = boost::mpl;
+namespace sc = boost::statechart;
+namespace mpl = boost::mpl;
 
-// // 事件定义
-// struct EvPowerOn : sc::event<EvPowerOn> {};
-// struct EvPowerOff : sc::event<EvPowerOff> {};
-// struct EvPlay : sc::event<EvPlay> {};
-// struct EvPause : sc::event<EvPause> {};
-// struct EvStop : sc::event<EvStop> {};
-// struct EvMute : sc::event<EvMute> {};
-// struct EvUnmute : sc::event<EvUnmute> {};
+// 事件定义
+struct EvPowerOn : sc::event<EvPowerOn> {};
+struct EvPowerOff : sc::event<EvPowerOff> {};
+struct EvPlay : sc::event<EvPlay> {};
+struct EvPause : sc::event<EvPause> {};
+struct EvStop : sc::event<EvStop> {};
+struct EvMute : sc::event<EvMute> {};
+struct EvUnmute : sc::event<EvUnmute> {};
 
-// // 前向声明
-// struct MediaPlayer;
-// struct Powered;
-// struct PoweredOn;
-// struct PoweredOff;
-// struct PlaybackRegion;
-// struct Playing;
-// struct Paused;
-// struct Stopped;
-// struct AudioRegion;
-// struct Muted;
-// struct Unmuted;
+struct EvPowertest : sc::event<EvPowertest> {};
 
-// // 媒体播放器状态机
-// struct MediaPlayer : sc::state_machine<MediaPlayer, Powered> {};
+// 前向声明
+struct MediaPlayer;
+struct Powered;
+struct PoweredOn;
+struct PoweredOff;
+struct PlaybackRegion;
+struct Playing;
+struct Paused;
+struct Stopped;
+struct AudioRegion;
+struct Muted;
+struct Unmuted;
 
-// // 顶层状态：电源状态
-// struct Powered : sc::simple_state<Powered, MediaPlayer, PoweredOff> {
-//     typedef sc::transition<EvPowerOn, PoweredOn> reactions;
-// };
+struct PowereTest;
 
-// // 电源关闭状态
-// struct PoweredOff : sc::simple_state<PoweredOff, Powered> {
-//     void entry() { std::cout << "进入电源关闭状态" << std::endl; }
-//     void exit() { std::cout << "离开电源关闭状态" << std::endl; }
-// };
+// 媒体播放器状态机
+struct MediaPlayer : sc::state_machine<MediaPlayer, Powered> {};
 
-// // 电源开启状态 - 包含两个正交区域
-// struct PoweredOn : sc::simple_state<PoweredOn, Powered, mpl::list<PlaybackRegion, AudioRegion>> {
-//     typedef sc::transition<EvPowerOff, PoweredOff> reactions;
+// 顶层状态：电源状态
+struct Powered : sc::simple_state<Powered, MediaPlayer, PoweredOff> {
+    typedef mpl::list<sc::transition<EvPowerOn, PoweredOn>
+        // sc::transition<EvPowerOff, PoweredOff>
+            > reactions;
+    // typedef sc::transition<EvPowerOn, PoweredOn> reactions;
+    Powered() {
+        std::cout << "Powered()" << std::endl;
+    }
+};
 
-//     void entry() { std::cout << "进入电源开启状态" << std::endl; }
-//     void exit() { std::cout << "离开电源开启状态" << std::endl; }
-// };
+// 电源关闭状态
+struct PoweredOff : sc::simple_state<PoweredOff, Powered> {
+    PoweredOff() {
+        std::cout << "PoweredOff()" << std::endl;
+    }
+    void entry() {
+        std::cout << "进入电源关闭状态" << std::endl;
+    }
+    void exit() {
+        std::cout << "离开电源关闭状态" << std::endl;
+    }
+};
 
-// // 播放控制区域
-// struct PlaybackRegion : sc::simple_state<PlaybackRegion, PoweredOn::orthogonal<0>, Stopped> {
-//     void entry() { std::cout << "初始化播放控制区域" << std::endl; }
-// };
+// 电源开启状态 - 包含两个正交区域
+struct PoweredOn : sc::simple_state<PoweredOn, Powered, mpl::list<PlaybackRegion, AudioRegion>> {
+    typedef mpl::list< sc::transition<EvPowerOff, PoweredOff>
+                      /*,sc::transition<EvPowertest, PowereTest>*/ // 会离开所有正交区域，所以只能在正交区域状态转换
+                      > reactions;
 
-// // 播放状态
-// struct Playing : sc::simple_state<Playing, PlaybackRegion> {
-//     typedef mpl::list<
-//         sc::transition<EvPause, Paused>,
-//         sc::transition<EvStop, Stopped>
-//         > reactions;
+    void entry() { std::cout << "进入电源开启状态" << std::endl; }
+    void exit() { std::cout << "离开电源开启状态" << std::endl; }
+};
 
-//     void entry() { std::cout << "进入播放状态" << std::endl; }
-//     void exit() { std::cout << "离开播放状态" << std::endl; }
-// };
+struct PowereTest: sc::simple_state<PowereTest , PoweredOn> {
+    void entry() { std::cout << "进入PowereTest状态" << std::endl; }
+    void exit() { std::cout << "离开PowereTest状态" << std::endl; }
+};
 
-// // 暂停状态
-// struct Paused : sc::simple_state<Paused, PlaybackRegion> {
-//     typedef mpl::list<
-//         sc::transition<EvPlay, Playing>,
-//         sc::transition<EvStop, Stopped>
-//         > reactions;
+// 播放控制区域
+struct PlaybackRegion : sc::simple_state<PlaybackRegion, PoweredOn::orthogonal<0>, Stopped> {
+    void entry() { std::cout << "初始化播放控制区域" << std::endl; }
+};
 
-//     void entry() { std::cout << "进入暂停状态" << std::endl; }
-//     void exit() { std::cout << "离开暂停状态" << std::endl; }
-// };
+// 播放状态
+struct Playing : sc::simple_state<Playing, PlaybackRegion> {
+    typedef mpl::list<
+        sc::transition<EvPause, Paused>,
+        sc::transition<EvStop, Stopped>
+        > reactions;
 
-// // 停止状态
-// struct Stopped : sc::simple_state<Stopped, PlaybackRegion> {
-//     typedef sc::transition<EvPlay, Playing> reactions;
+    void entry() { std::cout << "进入播放状态" << std::endl; }
+    void exit() { std::cout << "离开播放状态" << std::endl; }
+};
 
-//     void entry() { std::cout << "进入停止状态" << std::endl; }
-//     void exit() { std::cout << "离开停止状态" << std::endl; }
-// };
+// 暂停状态
+struct Paused : sc::simple_state<Paused, PlaybackRegion> {
+    typedef mpl::list<
+        sc::transition<EvPlay, Playing>,
+        sc::transition<EvStop, Stopped>
+        > reactions;
 
-// // 音频控制区域
-// struct AudioRegion : sc::simple_state<AudioRegion, PoweredOn::orthogonal<1>, Unmuted> {
-//     void entry() { std::cout << "初始化音频控制区域" << std::endl; }
-// };
+    void entry() { std::cout << "进入暂停状态" << std::endl; }
+    void exit() { std::cout << "离开暂停状态" << std::endl; }
+};
 
-// // 静音状态
-// struct Muted : sc::simple_state<Muted, AudioRegion> {
-//     typedef sc::transition<EvUnmute, Unmuted> reactions;
+// 停止状态
+struct Stopped : sc::simple_state<Stopped, PlaybackRegion> {
+    typedef sc::transition<EvPlay, Playing> reactions;
+    Stopped() { std::cout << "Stopped()" << std::endl; }
+    void entry() { std::cout << "进入停止状态" << std::endl; }
+    void exit() { std::cout << "离开停止状态" << std::endl; }
+};
 
-//     void entry() { std::cout << "进入静音状态" << std::endl; }
-//     void exit() { std::cout << "离开静音状态" << std::endl; }
-// };
+// 音频控制区域
+struct AudioRegion : sc::simple_state<AudioRegion, PoweredOn::orthogonal<1>, Unmuted> {
+    void entry() { std::cout << "初始化音频控制区域" << std::endl; }
+};
 
-// // 有声状态
-// struct Unmuted : sc::simple_state<Unmuted, AudioRegion> {
-//     typedef sc::transition<EvMute, Muted> reactions;
+// 静音状态
+struct Muted : sc::simple_state<Muted, AudioRegion> {
+    typedef sc::transition<EvUnmute, Unmuted> reactions;
 
-//     void entry() { std::cout << "进入有声状态" << std::endl; }
-//     void exit() { std::cout << "离开有声状态" << std::endl; }
-// };
+    void entry() { std::cout << "进入静音状态" << std::endl; }
+    void exit() { std::cout << "离开静音状态" << std::endl; }
+};
 
-// // 主函数
-// int main() {
-//     MediaPlayer player;
+// 有声状态
+struct Unmuted : sc::simple_state<Unmuted, AudioRegion> {
+    typedef sc::transition<EvMute, Muted> reactions;
 
-//     std::cout << "初始化媒体播放器状态机...\n" << std::endl;
-//     player.initiate();
+    void entry() { std::cout << "进入有声状态" << std::endl; }
+    void exit() { std::cout << "离开有声状态" << std::endl; }
+};
 
-//     std::cout << "\n打开电源..." << std::endl;
-//     player.process_event(EvPowerOn());
+// 主函数
+int main() {
+    MediaPlayer player;
 
-//     std::cout << "\n开始播放..." << std::endl;
-//     player.process_event(EvPlay());
+    std::cout << "初始化媒体播放器状态机...\n" << std::endl;
+    player.initiate();
 
-//     std::cout << "\n静音..." << std::endl;
-//     player.process_event(EvMute());
+    std::cout << "\n打开电源..." << std::endl;
+    player.process_event(EvPowerOn());
 
-//     std::cout << "\n暂停播放..." << std::endl;
-//     player.process_event(EvPause());
+    std::cout << "\n开始播放..." << std::endl;
+    player.process_event(EvPlay());
 
-//     std::cout << "\n取消静音..." << std::endl;
-//     player.process_event(EvUnmute());
+    std::cout << "\n静音..." << std::endl;
+    player.process_event(EvMute());
 
-//     std::cout << "\n停止播放..." << std::endl;
-//     player.process_event(EvStop());
+    // std::cout << "\nEvpowerTest..." << std::endl;
+    // player.process_event(EvPowertest());
 
-//     std::cout << "\n关闭电源..." << std::endl;
-//     player.process_event(EvPowerOff());
+    std::cout << "\n暂停播放..." << std::endl;
+    player.process_event(EvPause());
 
-//     return 0;
-// }
+    std::cout << "\n取消静音..." << std::endl;
+    player.process_event(EvUnmute());
+
+    std::cout << "\n停止播放..." << std::endl;
+    player.process_event(EvStop());
+
+    std::cout << "\n关闭电源..." << std::endl;
+    player.process_event(EvPowerOff());
+
+    while (1) {
+
+    }
+    return 0;
+}
